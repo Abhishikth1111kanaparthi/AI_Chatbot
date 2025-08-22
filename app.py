d0c7385e3173d6415e710325cf375f4e96c03814
Abhishikth1111kanaparthi <99632306+Abhishikth1111kanaparthi@users.noreply.github.com>
2025-08-22 20:07:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,29 @@ from gtts import gTTS
 import google.generativeai as genai
 import tempfile
 
+# getting gemini api key 
 genai.configure(api_key="AIzaSyAIrU13BmlwB8NwX9PpNZ411nwh7J884dw")
 
-st.set_page_config(page_title="Voice Chat with Gemini", page_icon="🎙️")
+# desgining the web page 
+st.set_page_config(page_title="Voice Chat with Gemini", page_icon="🎙️") # page title 
 st.title("🎙️ AI Chatbot")
 st.write("Speak into your microphone and hear Chatbot respond!")
 
-if "history" not in st.session_state:
+if "history" not in st.session_state: # saving chat history 
     st.session_state.history = []
 
-# Browser microphone capture
-audio_data = st.audio_input("🎤 Record 5s Audio")
+# Using Browser microphone capture audio
+audio_data = st.audio_input("🎤 Record Audio")
 
 if audio_data is not None:
     st.info("Processing your voice...")
 
-    # Save the browser audio to a temp WAV file
+    # Saving the browser audio to a temp WAV file
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_file:
         tmp_file.write(audio_data.getvalue())
         tmp_path = tmp_file.name
 
-    # Speech-to-Text
+    # Speech-to-Text STT converting
     r = sr.Recognizer()
     with sr.AudioFile(tmp_path) as source:
         audio_content = r.record(source)
@@ -36,23 +38,24 @@ if audio_data is not None:
             user_text = None
 
     if user_text:
-        # Gemini response
-        model = genai.GenerativeModel("gemini-2.5-flash")
+        # Gemini response 
+        model = genai.GenerativeModel("gemini-2.5-flash") # using gemini 2.5 flash model 
         response = model.generate_content(user_text)
         bot_reply = response.text
         st.write("**Gemini says:**", bot_reply)
 
-        # Text-to-Speech
+        # Text-to-Speech TTS conversion
         tts = gTTS(bot_reply)
         tts.save("reply.mp3")
         st.audio("reply.mp3", format="audio/mp3")
 
         st.session_state.history.append((user_text, bot_reply))
 
-# Display chat history
+# Displaying chat history
 if st.session_state.history:
     st.subheader("Chat History")
     for i, (q, a) in enumerate(st.session_state.history, 1):
         st.markdown(f"**You:** {q}")
         st.markdown(f"**Gemini:** {a}")
         st.markdown("---")
+
